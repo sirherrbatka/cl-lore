aa997307a83705dce15f6bb6702a36343b29833b
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-06-18 19:55:03 +0200

    Add some reasonable padding around function documentation.

--- src/html/style.lisp
+++ src/html/style.lisp
@@ -3,5 +3,9 @@
 
 (defparameter *function-style*
   '(.function-info
-    :border 1px solid "#A9A9A9"))
+    :border 1px solid "#A9A9A9"
+    :padding-top 3px
+    :padding-right 3px
+    :padding-bottom 3px
+    :padding-left 3px))
 
