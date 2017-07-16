1211d2fe49222c8b64cefcc5c35dcd94ad50787d
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-07-16 18:20:51 +0200

    Restore *register* logic.

--- src/api/raw/implementation.lisp
+++ src/api/raw/implementation.lisp
@@ -1,9 +1,19 @@
 (in-package #:cl-lore.api.raw)
 
 
+(defmethod cl-lore.protocol.stack:controller-return
+    :after ((controller cl-lore.protocol.stack:fundamental-stack-controller)
+            value)
+  (setf *register* value))
+
+
 (defmethod cl-lore.protocol.stack:controller-push-tree
     :after ((controller cl-lore.protocol.stack:fundamental-stack-controller)
             description
             value)
   (setf *register* value))
 
+
+(defmethod cl-lore.protocol.stack:controller-return ((controller (eql nil)) value)
+  (setf *register* value)
+  value)
