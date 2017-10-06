eca88920642081486bd7d4cc8f7ac3f1a36b044d
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-10-06 20:02:50 +0200

    Corrected restart-case.

--- src/api/syntax/control.lisp
+++ src/api/syntax/control.lisp
@@ -1,25 +1,26 @@
 (in-package #:cl-lore.api.syntax)
 
 
-(defmacro define-save-output-function (name)
+(defmacro define-save-output-function (name document-form)
   (with-gensyms (!path)
     `(let ((,!path nil))
-       (defun ,name (document &optional path)
-         (declare (type (or null string pathname) path)
-                  (type cl-lore.protocol.output:fundamental-output
-                        document))
-         (tagbody
-          :start
-            (check-type path (or null string pathname))
-            (unless (null path)
-              (setf ,!path (pathname path)))
-            (flet ((set-path (e)
-                     (declare (ignorable e))
-                     (setf path (read)
-                           ,!path nil)
-                     (go :start)))
-              (handler-bind ((file-error #'set-path))
-                (when (null ,!path)
-                  (error 'file-error))
-                (cl-lore.protocol.output:save-output ,!path
-                                                     document))))))))
+       (defun ,name (&optional path)
+         (declare (type (or null string pathname) path))
+         (check-type path (or null string pathname))
+         (unless (null path)
+           (setf ,!path (pathname path)))
+         (restart-case
+             (progn
+               (when (null ,!path)
+                 (error "Path was not set!"))
+               (cl-lore.protocol.output:save-output
+                ,!path
+                (assure cl-lore.protocol.output:fundamental-output ,document-form)))
+           (set-path (path)
+             :report "Set path."
+             :interactive (lambda ()
+                            (format *query-io* "Enter new path.~%")
+                            (force-output *query-io*)
+                            (list (read)))
+             (,name path)))
+         ,!path))))
