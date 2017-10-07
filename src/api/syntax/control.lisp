3a634550241de415afcfbc4b1950e3de83d5c207
shka <sirherrbatka@gmail.com>
2017-10-07 11:36:26 +0200

    Add option to load files in custom positions.

--- src/api/syntax/control.lisp
+++ src/api/syntax/control.lisp
@@ -1,14 +1,22 @@
 (in-package #:cl-lore.api.syntax)
 
 
-(defmacro define-save-output-function (name document-form)
-  (with-gensyms (!path)
-    `(let ((,!path nil))
+(defmacro define-save-output-function (name (&rest files)
+                                       document-form)
+  (with-gensyms (!path !current-path)
+    `(let ((,!path nil)
+           (,!current-path (uiop/pathname:pathname-directory-pathname
+                            (asdf-utils:current-lisp-file-pathname))))
        (defun ,name (&optional path)
          (declare (type (or null string pathname) path))
          (check-type path (or null string pathname))
          (unless (null path)
            (setf ,!path (pathname path)))
+         (map nil (lambda (file)
+                    (let ((full-path (uiop/pathname:merge-pathnames*
+                                      ,!current-path file)))
+                      (load full-path)))
+              (list ,@files))
          (restart-case
              (progn
                (when (null ,!path)
