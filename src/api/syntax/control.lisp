199dc8beaa367140c61c7f6f0a4df483b2dcefe7
Marek Kochanowicz <sirherrbatka@gmail.com>
2017-11-12 16:00:36 +0100

    Also enable passing options for dynamic binding.

--- src/api/syntax/control.lisp
+++ src/api/syntax/control.lisp
@@ -4,8 +4,8 @@
 (defmacro define-save-output-function (name
                                        (names
                                         generator
-                                        chunks
-                                        &key output-options)
+                                        chunks)
+                                       (&key output-options dynamic-binding)
                                        (&rest files)
                                        &body document-form)
   (with-gensyms (!path !output !current-path)
@@ -17,29 +17,32 @@
          (check-type path (or null list string pathname))
          (unless (null path)
            (setf ,!path (uiop:make-pathname* :directory path)))
-         (map nil (lambda (file)
-                    (let ((full-path (uiop/pathname:merge-pathnames*
-                                      ,!current-path file)))
-                      (load full-path)))
-              (list ,@files))
-         (restart-case
-             (progn
-               (when (null ,!path)
-                 (error "Path was not set!"))
-               (unless (uiop:directory-exists-p ,!path)
-                 (error "Directory does not exists!"))
-               (cl-lore.protocol.output:save-output
-                ,!path
-                (with-names ,names
-                  (cl-lore.api.syntax:document
-                      (,generator ,!output ,chunks :output-options ,output-options)
-                    ,@document-form))))
-           (set-path (path)
-             :report "Set path."
-             :interactive (lambda ()
-                            (format *query-io* "Enter new path.~%")
-                            (force-output *query-io*)
-                            (list (read)))
-             (,name path)))
-         ,!path)
+         (,@(if dynamic-binding
+                `(let ,dynamic-binding)
+                `(progn))
+          (map nil (lambda (file)
+                     (let ((full-path (uiop/pathname:merge-pathnames*
+                                       ,!current-path file)))
+                       (load full-path)))
+               (list ,@files))
+          (restart-case
+              (progn
+                (when (null ,!path)
+                  (error "Path was not set!"))
+                (unless (uiop:directory-exists-p ,!path)
+                  (error "Directory does not exists!"))
+                (cl-lore.protocol.output:save-output
+                 ,!path
+                 (with-names ,names
+                   (cl-lore.api.syntax:document
+                       (,generator ,!output ,chunks :output-options ,output-options)
+                     ,@document-form))))
+            (set-path (path)
+              :report "Set path."
+              :interactive (lambda ()
+                             (format *query-io* "Enter new path.~%")
+                             (force-output *query-io*)
+                             (list (read)))
+              (,name path)))
+          ,!path))
        (export (quote ,name)))))
