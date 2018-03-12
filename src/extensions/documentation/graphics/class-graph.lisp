1054a07773547a94e3480dec9269da1cd02d280d
Marek Kochanowicz <sirherrbatka@gmail.com>
2018-03-12 22:18:54 +0100

    use onormal style of arrowhead.

--- src/extensions/documentation/graphics/class-graph.lisp
+++ src/extensions/documentation/graphics/class-graph.lisp
@@ -28,7 +28,7 @@
             (make-instance 'cl-dot:attributed
                            :object x
                            :attributes '(:weight 1
-                                         :arrowhead :none)))
+                                         :arrowhead :onormal)))
           (~>> (closer-mop:class-direct-superclasses object)
                (remove (find-class 'standard-object))
                (remove (find-class 'error))
